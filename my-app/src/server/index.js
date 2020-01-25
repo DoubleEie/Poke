4574c8d289c7a77b695a8ae9561c8cf92c576ead
Kaiwen Shen <kev.shen991206@gmail.com>
2020-01-25 17:38:21 -0500

    updated server and router

--- my-app/src/server/index.js
+++ my-app/src/server/index.js
@@ -1,11 +1,20 @@
-var app = require('http').createServer()
-var io = module.exports.io = require('socket.io')(app)
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io')
 
-const PORT = process.env.PORT || 3231
-const SocketManager = require('./SocketManager')
+const PORT = process.env.PORT || 5000
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+const router = require('./router');
 
-io.on('connection', SocketManager)
-
-app.listen(PORT, ()=>{
-    console.log("Connected to port" + PORT);
+io.on('connection', (socket)=>{
+    console.log('Connection success');
+    socket.on('disconnect', ()=>{
+        console.log('Disconnected');
+    })
 })
+
+app.use(router);
+
+server.listen(PORT, ()=> console.log(`Server has started on port ${PORT}`));
