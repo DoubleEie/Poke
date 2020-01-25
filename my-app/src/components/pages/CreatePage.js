c4ded05377cadc324bbb888dac8c5d6fad6ec742
Tim Zhou <tzhou17@gmail.com>
2020-01-25 16:18:39 -0500

    create/join pages

--- my-app/src/components/pages/CreatePage.js
+++ my-app/src/components/pages/CreatePage.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, {Component} from "react";
 import {Link} from "react-router-dom";
 import {Button} from 'reactstrap';
 import '../../App.css';
-const CreatePage = () =>{
+import { tsConstructorType } from "@babel/types";
+class CreatePage extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            code: null
+        }
+    }
+
+    handleSubmit = (event) =>{
+        event.preventDefault()
+        const data = this.state
+    }
+    handleInputChange = (event) => {
+        event.preventDefault()
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    render(){
+    const{code} = this.state
     return(
         <div className = "container">
-            <div className = "header">Create Code</div>
+             <div className = "header"><Link to = "/">
+                <button className = "back-button">Back</button></Link>
+            Create a Poke Space !</div>
+        <div className = "code-enter">
+            <form onSubmit = {this.handleSubmit}>
+                <p><input type = 'text' placeholder ='Poke Code' code = 'code'onChange = {this.handleInputChange}/></p>
+                <Link to = "/user">
+                <p className = "create-join-btn"><button>Create</button></p>
+                </Link>
+            </form>
+        </div>    
         </div>
-    );
-};
+    )
+}
+}
+
 
 export default CreatePage;
--- my-app/src/components/pages/JoinPage.js
+++ my-app/src/components/pages/JoinPage.js
@@ -1,13 +1,43 @@
-import React from "react";
+import React, {Component} from "react";
 import {Link} from "react-router-dom";
 import {Button} from 'reactstrap';
 import '../../App.css';
-const JoinPage = () =>{
+class JoinPage extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            code: null
+        }
+    }
+
+    handleSubmit = (event) =>{
+        event.preventDefault()
+        const data = this.state
+    }
+    handleInputChange = (event) => {
+        event.preventDefault()
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    render(){
+    const{code} = this.state
     return(
         <div className = "container">
-            <div className = "header">Join Code</div>
+            <div className = "header"><Link to = "/">
+                <button className = "back-button">Back</button></Link>
+            Join a Party !</div>
+            <div className = "code-enter">
+            <form onSubmit = {this.handleSubmit}>
+                <p><input type = 'text' placeholder ='Poke Code' code = 'code'onChange = {this.handleInputChange}/></p>
+                <Link to = "/">
+                <p className = "create-join-btn"><button>Join</button></p>
+                </Link>
+            </form>
+        </div>    
         </div>
     );
 };
+}
 
 export default JoinPage;
