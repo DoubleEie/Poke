cb13037cddd0bcd721733e4fa68951ea3a5295fe
Tim Zhou <tzhou17@gmail.com>
2020-01-25 22:54:50 -0500

    Front page image addition

--- my-app/src/components/pages/WelcomePage.js
+++ my-app/src/components/pages/WelcomePage.js
@@ -2,10 +2,14 @@ import React from "react";
 import {Link} from "react-router-dom";
 import {Button} from 'reactstrap';
 import '../../App.css';
+import logo from '../../images/PokeWelcomeScreenLogo.png';
 const WelcomePage = () =>{
     return(
         <div className = "container">
             <div className = "header">Welcome to Poke</div>
+            <div className = 'image-container'>
+                <img src = {logo} alt="cannot display"/>
+            </div>
                 <div className = "button-contain">
                     <div className = "btn-group">
                         <Link to = "/create">
